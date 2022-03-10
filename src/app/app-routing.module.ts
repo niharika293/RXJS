7061e3c4711510e5748158a3295527a19cd4ff3d
Niharika Gurnani <niharika.gurnani@solvezy.com>
2022-03-10 12:17:34 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PromiseComponent } from './includes/promise/promise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'promise', component : PromiseComponent},
+  {path : '**', redirectTo : 'promise'} //This will redirect any unmatched /non-existing route to promise component.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './includes/header/header.component';
+import { PromiseComponent } from './includes/promise/promise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PromiseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
